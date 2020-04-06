8b98b47d90d596833835f942eae65997b5e8251d
Prasad Patil <prasadpatil131@gmail.com>
2020-04-06 17:18:53 +0530

    Update main_.py

--- main_.py
+++ main_.py
@@ -1,16 +1,33 @@
 import json
+from pymongo import MongoClient
 from difflib import get_close_matches
 
-data = json.load(open("data.json"))
+client = MongoClient('localhost', 27017)
+
+db = client['dictionary']                 # connecting to database
+
+db_collc = db['collc']                    # connecting to collection
+
+with open('data.json') as f:              # loading json file
+    data = json.load(f)
+    
+                                          # replace dot present in key
+final_data = {x.replace('.', ''): v for x, v in data.items()} 
+
+db_collc.insert(final_data)               # insert data into database
+
+for record in db_collc.find({}):          # retrieving data from databse
+   		continue
+
 
 def translate(w):
     w = w.lower()
-    if w in data:
-        return data[w]
+    if w in record:
+        return record[w]
     elif len(get_close_matches(w, data.keys())) > 0:
         yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(w, data.keys())[0])
         if yn == "Y":
-            return data[get_close_matches(w, data.keys())[0]]
+            return record[get_close_matches(w, record.keys())[0]]
         elif yn == "N":
             return "The word doesn't exist. Please double check it."
         else:
