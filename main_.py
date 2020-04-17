420e8eac8cdb8d1959742ea979ded1eaaece72a4
Prasad Patil <prasadpatil131@gmail.com>
2020-04-17 22:41:30 +0530

    Update main_.py

--- main_.py
+++ main_.py
@@ -10,15 +10,13 @@ db_collc = db['collc']                    # connecting to collection
 
 with open('data.json') as f:              # loading json file
     data = json.load(f)
-    
                                           # replace dot present in key
 final_data = {x.replace('.', ''): v for x, v in data.items()} 
 
-db_collc.insert(final_data)               # insert data into database
+db_collc.insert_one(final_data)           # insert data into database
 
 for record in db_collc.find({}):          # retrieving data from databse
    		continue
-
 
 def translate(w):
     w = w.lower()
@@ -26,14 +24,15 @@ def translate(w):
         return record[w]
     elif len(get_close_matches(w, data.keys())) > 0:
         yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(w, data.keys())[0])
-        if yn == "Y":
+        if yn == "Y" or "y":
             return record[get_close_matches(w, record.keys())[0]]
-        elif yn == "N":
+        elif yn == "N" or "n":
             return "The word doesn't exist. Please double check it."
         else:
             return "We didn't understand your entry."
     else:
         return "The word doesn't exist. Please double check it."
+    
 
 word = input("Enter word: ")
 output = translate(word)
